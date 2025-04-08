0a54e239f4c1a5b221d27e7f17b527ae57966402
Sabarinathan R <r.sabarinathan02@gmail.com>
2025-04-08 12:54:51 +0530

    learn go hello world

---
+++ hello-world.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	// Print a hello world message
+	fmt.Println("Hello, World!")
+
+	// Log a message
+	log.Println("This is a log message.")
+}
