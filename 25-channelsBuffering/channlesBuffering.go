dc7e79fe4bf46b6ad2c4557f25b7d9fd2d81a865
Sabarinathan R <r.sabarinathan02@gmail.com>
2025-06-07 00:47:19 +0530

    feat: added channels with buffering updated

--- 25-channelsBuffering/channlesBuffering.go
+++ 25-channelsBuffering/channlesBuffering.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func main() {
 
-	messages := make(chan string, 2)
+	messages := make(chan string, 3)
 
 	messages <- "buffered"
 	messages <- "channel"
+	messages <- "extra" // This will cause a deadlock since the channel is full	
 
 	fmt.Println(<-messages)
 	fmt.Println(<-messages)
+	fmt.Println(<-messages) // This line will not be reached due to the deadlock
 }
